9f6fe5a5fe21ecfb6cda73993a660a062ee66093
ToppleKek <braebear85@gmail.com>
2018-11-17 14:04:33 -0500

    kicking fixes

--- commands/dabon.js
+++ commands/dabon.js
@@ -35,39 +35,33 @@ module.exports = {
         if (targetPos >= authorPos && msg.author.id !== msg.guild.ownerID) utils.sendResponse(msg, `${targetUsr.tag} has a higher or equal role. You also cannot dab on yourself`, 'err');
         else if (!target.kickable) utils.sendResponse(msg, `Missing permissions to dab on ${targetUsr.tag}`, 'err');
         else {
-          target.createDM().then(chan => {
-            chan.send({
-              embed:{
-                color: 1571692,
-                title: `Get heckin dabbed on from ${msg.guild.name}`,
-                description: `They kicked you for \`${reason}\``,
-                timestamp: new Date(),
-              }
-            }).catch(err => {
-              utils.sendResponse(msg, `Failed to DM ${targetUsr.tag} with error ${err}, trying to ban anyway`, 'info');
-            });
-          }).catch(err => {
-            utils.sendResponse(msg, `Failed to DM ${targetUsr.tag} with error ${err}, trying to ban anyway`, 'info');
-          });
+          const chan = await target.createDM()
+                      .catch(err => utils.sendResponse(msg, `Failed to create DM with ${targetUsr.tag} with error ${err}, trying to kick anyway`, 'info'));
 
-          target.kick({ reason: reason })
-              .then(member => {
-                utils.writeToModlog(msg.guild.id, 'dabbed on (kick)', reason, targetUsr.tag, false, msg.author);
-                msg.channel.send({
-                  embed: {
-                    color: 1571692,
-                    title: `${targetUsr.tag} JUST GOT DABBED ON`,
-                    description: 'GET FRICKED KIDDO',
-                    thumbnail: {
-                      url: 'https://cdn.discordapp.com/attachments/298938210556575755/467035923289341965/breaddab.gif',
-                    },
-                    timestamp: new Date(),
-                  }
-                })
-              })
-              .catch(err => {
-                utils.sendResponse(msg, `Failed to dab. Error: ${err}`, 'err');
-              });
+          await chan.send({
+            embed:{
+              color: 1571692,
+              title: `Get heckin dabbed on from ${msg.guild.name}`,
+              description: `They kicked you for \`${reason}\``,
+              timestamp: new Date(),
+            }
+          }).catch(err => utils.sendResponse(msg, `Failed to DM ${targetUsr.tag} with error ${err}, trying to kick anyway`, 'info'));
+
+          const member = await target.kick({ reason: reason })
+                        .catch(err => utils.sendResponse(msg, `Failed to dab. Error: ${err}`, 'err'));
+
+          utils.writeToModlog(msg.guild.id, 'dabbed on (kick)', reason, targetUsr.tag, false, msg.author);
+          msg.channel.send({
+            embed: {
+              color: 1571692,
+              title: `${targetUsr.tag} JUST GOT DABBED ON`,
+              description: 'GET FRICKED KIDDO',
+              thumbnail: {
+                url: 'https://cdn.discordapp.com/attachments/298938210556575755/467035923289341965/breaddab.gif',
+              },
+              timestamp: new Date(),
+            }
+          });
         }
       }
       else {
