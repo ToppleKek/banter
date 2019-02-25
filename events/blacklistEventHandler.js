01799edcb7648d1fcb5b58099594f698aad441a8
ToppleKek <braebear85@gmail.com>
2019-02-25 15:13:04 -0500

    fix for zero-width spaces in the blacklist
    im gonna have to clean this up at some point

--- events/blacklistEventHandler.js
+++ events/blacklistEventHandler.js
@@ -5,27 +5,28 @@ const configTools = require('../utils/configTools.js');
 module.exports = {
   message: async (placeholder, message) => {
     if (!message.guild || message.author.bot) return;
-    const ingoredChannels = await utils.getIgnoredChannels(message.guild.id, 'blacklist')
+    const guild = message.guild;
+    const ingoredChannels = await utils.getIgnoredChannels(guild.id, 'blacklist')
                             .catch(err => console.log(`[DEBUG] Failed to get ingoredChannels ${err}`));
     if (ingoredChannels) {
       const arr = ingoredChannels.split(' ');
       if (arr.includes(message.channel.id)) return;
     }
     const isAdmin = await utils.checkPermission(message.author, message, 'admin');
-    if (message.guild.member(message.author) && isAdmin && message.content.startsWith(`${CONFIG.prefix}blacklist`)) return;
-    let conf = await configTools.getConfig(message.guild)
+    if (guild.member(message.author) && isAdmin && message.content.startsWith(`${CONFIG.prefix}blacklist`)) return;
+    let conf = await configTools.getConfig(guild)
         .catch(err => console.log(err));
     // if (!conf) conf = CONFIG.defaultConfig;
     // conf = configTools.decodeConfig(conf);
     if (!(conf instanceof Error)) {
-      console.log(`[DEBUG] Ignoring admins? ${conf.blIgnoreAdmins} Guild: ${message.guild.name}`);
+      console.log(`[DEBUG] Ignoring admins? ${conf.blIgnoreAdmins} Guild: ${guild.name}`);
       if (conf.blIgnoreAdmins && isAdmin) return;
     }
-    mainModule.db.get(`SELECT blacklist FROM servers WHERE id = ${message.guild.id}`, async (err, row) => {
+    mainModule.db.get(`SELECT blacklist FROM servers WHERE id = ${guild.id}`, async (err, row) => {
       if (err) return console.log(`[ERROR] blacklistEventHandler: message: ${err}`);
       if (row && row.blacklist) {
         let toCheck;
-        let conf = await configTools.getConfig(message.guild)
+        let conf = await configTools.getConfig(guild)
             .catch(err => console.log(err));
         // if (!conf) conf = CONFIG.defaultConfig;
         // conf = configTools.decodeConfig(conf);
@@ -35,16 +36,16 @@ module.exports = {
               toCheck = message.content;
               break;
             case 1:
-              toCheck = message.content.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[*`_​]/g, '');
+              toCheck = message.content.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[*`_​]/g, '').replace("⁣", '');
               break;
             case 2:
-              toCheck = message.content.toLowerCase().replace(/\s/g, '').normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[*`_​]/g, '');
+              toCheck = message.content.toLowerCase().replace(/\s/g, '').normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[*`_​]/g, '').replace("⁣", '');
               break;
             default:
-              toCheck = message.content.toLowerCase().replace(/\s/g, '').normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[*`_​]/g, '');
+              toCheck = message.content.toLowerCase().replace(/\s/g, '').normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[*`_​]/g, '').replace("⁣", '');
           }
         } else {
-          console.log(`[ERROR] conf is instanceof Error: ${conf} Guild: ${message.guild.name}`);
+          console.log(`[ERROR] conf is instanceof Error: ${conf} Guild: ${guild.name}`);
         }
         const bWords = row.blacklist.toLowerCase().split(' ');
         const infractions = [];
@@ -65,10 +66,9 @@ module.exports = {
 
           message.delete({reason: 'Word filter broken'})
               .then(async msg => {
-                const guild = message.guild;
-                const member = message.guild.member(message.author);
+                const member = guild.member(message.author);
                 const fields = [];
-                let conf = await configTools.getConfig(message.guild)
+                let conf = await configTools.getConfig(guild)
                     .catch(err => console.log(err));
                 if (!(conf instanceof Error)) {
                   if (conf.blShowInfractions) {
@@ -82,7 +82,7 @@ module.exports = {
                   }
                 }
                 utils.timedMute(member.user.id, member.guild.id, conf.bmLen * 60, true, mainModule.client.user, 'Word filter infraction', false, true);
-                utils.writeToModlog(msg.guild.id, `timed mute for ${conf.bmLen} minutes`, 'word filter broken', member.user.tag, true, mainModule.client.user.tag, 0, fields);
+                utils.writeToModlog(guild.id, `timed mute for ${conf.bmLen} minutes`, 'word filter broken', member.user.tag, true, mainModule.client.user.tag, 0, fields);
                 member.createDM()
                     .then(chan => {
                       chan.send({
