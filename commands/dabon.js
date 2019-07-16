077b3e2834b4756b9329796cd200d491786984d3
ToppleKek <braebear85@gmail.com>
2019-07-16 14:12:28 -0400

    refactor dabon

--- commands/dabon.js
+++ commands/dabon.js
@@ -1,86 +1,75 @@
 const utils = require('../utils/utils.js');
 const CONFIG = require('../config.json');
+
 module.exports = {
-  help: 'dabon (kick) a user from the server',
-  usage: `${CONFIG.prefix}dabon @someone reason for kick`,
-  main: async (client, msg, hasArgs) => {
-    const hasKICK = await utils.checkPermission(msg.author, msg, 'kick');
-    const hasMR = await utils.checkPermission(msg.author, msg, 'admin');
-    if (hasMR || hasKICK) {
-      if (hasArgs) {
-        let target;
+    help: 'dabon (kick) a user from the server',
+    usage: `${CONFIG.prefix}dabon @someone reason for kick`,
+    main: async (client, msg, hasArgs) => {
+        if (!await utils.checkPermission(msg.author, msg, 'kick') || !await utils.checkPermission(msg.author, msg, 'admin'))
+          return utils.commandError(msg, 'Permission Error', 'You must have permission to kick members to execute this command', module.exports.usage);
+
+        if (!hasArgs)
+          return utils.commandError(msg, 'Argument Error', 'You must provide a mention or userID of the person you want to kick', module.exports.usage);
+        
+        msg.content = msg.content.replace(/\s+/g, ' ');
+
         const args = msg.content.split(' ');
         const userArg = args[0];
-        let reason;
+        let reason = 'No reason provided';
+        let target;
+
         if (args[1]) {
           args.shift();
           reason = args.join(' ');
         }
-        else reason = 'No reason provided';
-        console.log(userArg);
-        if (userArg === '@everyone' || userArg === '@here') return utils.sendResponse(msg, 'What am I gonna do?! Mintz the server ***again***????', 'err');
-        if (msg.mentions.users.first())
+
+        if (msg.mentions.users.first() && !msg.guild.member(msg.mentions.users.first()))
+          return utils.commandError(msg, 'Argument Error', 'The user specified does not exist in the guild (Guild.member returned null)', module.exports.usage);
+        else if (msg.mentions.users.first() && msg.guild.member(msg.mentions.users.first()))
           target = msg.guild.member(msg.mentions.users.first());
-        
+
         if (!target) {
-          let usr = await client.users.fetch(userArg).catch(err => utils.sendResponse(msg, `Failed to get user: ${err}`, 'err'));
+          target = await client.users.fetch(userArg).catch(err => 
+            utils.commandError(msg, 'Argument Error', `"${userArg}" is not a valid user (err: ${err})`, module.exports.usage));
 
-          if (usr && msg.guild.member(usr))
-            target = msg.guild.member(usr);
-          else if (usr)
-            return utils.sendResponse(msg, 'User not in guild!', 'err');
-        } 
+          if (!target)
+            return;
+          else if (!msg.guild.member(target))
+            return utils.commandError(msg, 'Argument Error', 'The user specified does not exist in the guild (Guild.member returned null)', module.exports.usage);
 
-        if (!target)
-          return utils.sendResponse(msg, `Invalid user\nUsage: ${module.exports.usage}`, 'err');
+          target = msg.guild.member(target);
+        }
 
+        if ((utils.getHighestRolePos(msg.guild.id, target) >= utils.getHighestRolePos(msg.guild.id, msg.author)) && msg.author.id !== msg.guild.ownerID)
+          return utils.commandError(msg, 'Permission Error', `${target.user.tag} has a higher or equal role. You also cannot dabon yourself`, module.exports.usage);
 
-        const targetPos = utils.getHighestRolePos(msg.guild.id, target);
-        const authorPos = utils.getHighestRolePos(msg.guild.id, msg.author);
-        const targetUsr = target.user;
+        const chan = await target.createDM().catch(err => utils.sendResponse(msg, 'Warning: Cannot send direct messages to the target user', 'info'));
 
-        // Back by popular demand
-        if (target.id === client.user.id) return utils.sendResponse(msg, 'How do I dab on myself???!!?!??', 'err');
-        else if (target.id === CONFIG.ownerid) return utils.sendResponse(msg, 'This guy is god!!!!!1 Why would I dab on him??!??!', 'err');
-        else if (target.id === msg.guild.ownerID) return utils.sendResponse(msg, 'Damn that persons the owner no dab', 'err');
-
-        if (targetPos >= authorPos && msg.author.id !== msg.guild.ownerID) utils.sendResponse(msg, `${targetUsr.tag} has a higher or equal role. You also cannot dab on yourself`, 'err');
-        else if (!target.kickable) utils.sendResponse(msg, `Missing permissions to dab on ${targetUsr.tag}`, 'err');
-        else {
-          const chan = await target.createDM()
-                      .catch(err => utils.sendResponse(msg, `Failed to create DM with ${targetUsr.tag} with error ${err}, trying to kick anyway`, 'info'));
-
+        if (chan && target.bannable) {
           await chan.send({
-            embed:{
+            embed: {
               color: 1571692,
               title: `Get heckin dabbed on from ${msg.guild.name}`,
               description: `They kicked you for \`${reason}\``,
               timestamp: new Date(),
             }
-          }).catch(err => utils.sendResponse(msg, `Failed to DM ${targetUsr.tag} with error ${err}, trying to kick anyway`, 'info'));
-
-          const member = await target.kick({ reason: reason })
-                        .catch(err => utils.sendResponse(msg, `Failed to dab. Error: ${err}`, 'err'));
-
-          utils.writeToModlog(msg.guild.id, 'dabbed on (kick)', reason, targetUsr.tag, false, msg.author);
-          msg.channel.send({
-            embed: {
-              color: 1571692,
-              title: `${targetUsr.tag} JUST GOT DABBED ON`,
-              description: 'GET FRICKED KIDDO',
-              thumbnail: {
-                url: 'https://cdn.discordapp.com/attachments/298938210556575755/467035923289341965/breaddab.gif',
-              },
-              timestamp: new Date(),
-            }
-          });
+          }).catch(err => utils.sendResponse(msg, `Warning: Failed to DM user: ${err}`, 'info'));
         }
-      }
-      else {
-        utils.sendResponse(msg, `You must provide a mention or a userID to dab on!\nUsage: ${module.exports.usage}`, 'err');
-      }
-    } else {
-      utils.sendResponse(msg, 'You must have permission to kick members to execute this command', 'err');
+
+        if (!await target.kick({reason: reason}).catch(err => utils.sendResponse(msg, `Could not kick ${target.user.tag}! Error: ${err}`, 'err')))
+            return;
+
+        utils.writeToModlog(msg.guild.id, 'dabbed on (kick)', reason, target.user.tag, false, msg.author);
+        msg.channel.send({
+          embed: {
+            color: 1571692,
+            title: `${target.user.tag} JUST GOT DABBED ON`,
+            description: 'GET FRICKED KIDDO',
+            thumbnail: {
+              url: 'https://cdn.discordapp.com/attachments/298938210556575755/467035923289341965/breaddab.gif',
+            },
+            timestamp: new Date(),
+          }
+        });
     }
-  }
 };
